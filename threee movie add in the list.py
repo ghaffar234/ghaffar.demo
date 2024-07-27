d3dce2505886ff568f270e52509ca64283843304
Abdul ghaffar <bajwaghaffar269@gmail.com>
2024-07-27 19:03:57 +0500

    Add files via upload

---
+++ threee movie add in the list.py
@@ -0,0 +1,10 @@
+#make the code for take input three movie names and add into the list
+user1=input("movie name = ")
+user2=input("movie name = ")
+user3=input("movie name = ")
+list=[]
+list.append(user1)
+list.append(user2)
+list.append(user3)
+print(list)
+

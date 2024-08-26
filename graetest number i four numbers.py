1b12c2c4dfe0bf3ae84f92bc24533ddcd6e25bf2
ghaffar234 <bajwaghaffar269@gmail.com>
2024-08-26 13:44:31 +0500

    add some lines

--- graetest number i four numbers.py
+++ graetest number i four numbers.py
@@ -8,5 +8,7 @@ elif(num2>num1 and num2>num3 and num2>num4):
     print("Num2 is a graetest number in the four numbers")  
 elif(num3>num1 and num3>num2 and num3>num4):
     print("Num4 is a greatest number in these four numbers")
+elif(num3==num1 and num3==num2 and num3==num4 and num4==num1):
+    print("Ghaffar is bad boy.")    
 else:
     print("num4 is a greatest number in these four numbers ")          
